de6ea5f7366ab0aaf8dcc5cbf449d6acd9e29132
Monorandoll-Im <monorandoll@csu.fullerton.edu>
2025-09-18 10:57:22 -0700

    Connected FrontEnd port to BackEnd, added navigation Bar file

--- inventory-ui/src/App.jsx
+++ inventory-ui/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [items, setItems] = useState([]);
+  const [err, setErr] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+  //proxy server setting for this can be edited in vite.config.js file 
+  //Target can be changed
+  useEffect(() => {
+    fetch("/weatherforecast") //Test enpoint
+      .then(r => { if (!r.ok) throw new Error(`${r.status} ${r.statusText}`); return r.json(); })
+      .then(data => setItems(data))
+      .catch(e => setErr(e.message));
+  }, []);
+
+
+
 }
-
-export default App
