4896b54418e32af76a4f245acaeb228555a21da3
Stephan Maready <stephan.maready@gmail.com>
2025-09-25 08:45:26 -0700

    Created the InventoryLocationQueryEndpoints.cs file that allows us to GET our data. Created the InventoryLocationCrudManualTests.http file.

---
+++ Backend/InventoryManagement.Api/Features/Location/Endpoints/InventoryLocationQueryEndpoints.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using InventoryManagement.Api.Database;
+using Microsoft.AspNetCore.Mvc;
+using InventoryManagement.Api.Features.Location.Commands;
+using InventoryManagement.Api.Features.Location.Validators;
+using InventoryManagement.Api.Features.Location.Handlers;
+
+
+namespace InventoryManagement.Api.Features.Location.Endpoints;
+
+// This code defines an endpoint for handling commands related to inventory locations in an inventory management system.
+// It uses ASP.NET Core's minimal APIs to create a POST endpoint for adding new inventory locations
+public static class InventoryLocationQueryEndpoints
+{
+
+    public static void MapInventoryLocationQueryEndpoints(this IEndpointRouteBuilder app)
+    {
+        // Create a route group for inventory location-related endpoints
+        var group = app.MapGroup("/api/locations")
+            .WithTags("Inventory Locations");
+
+
+        // Define a GET endpoint for retrieving all inventory locations
+        group.MapGet("/", () =>
+        {
+            var results = MockDataabase.InventoryLocations;
+            return Results.Ok(results);
+        })
+        .WithName("GetInventoryLocations")
+        .WithTags("InventoryLocations");
+
+        // Define a GET endpoint for retrieving a specific inventory location by its ID
+        group.MapGet("/{id:int}", (int id) =>
+        {
+            var result = MockDataabase.InventoryLocations.FirstOrDefault(loc => loc.Id == id);
+            return result is not null ? Results.Ok(result) : Results.NotFound();
+        })
+        .WithName("GetInventoryLocationById")
+        .WithTags("InventoryLocations");
+    }
+}
